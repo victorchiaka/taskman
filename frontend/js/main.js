d2075f002c340cc70b3471bc3afe949026400d0c
Victor Chiaka <chiakavictor205@gmail.com>
2023-10-07 23:47:15 +0100

    designed home page with animations

---
+++ frontend/js/main.js
@@ -0,0 +1,20 @@
+const showCase = document.querySelectorAll(".item-child");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      entry.target.classList.remove("hidden");
+    } else {
+      entry.target.classList.remove("show");
+      entry.target.classList.add("hidden");
+    }
+  });
+});
+
+showCase.forEach((element) => observer.observe(element));
+
+const navToggler = document.querySelector("#nav-button");
+navToggler.addEventListener("click", () => {
+  navToggler.classList.toggle("open-nav");
+});
