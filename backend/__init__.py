fe7c98fcb056a436ad2a4452efae78b4b13d84d5
Victor Chiaka <chiakavictor205@gmail.com>
2023-10-10 01:07:43 +0100

    designed signin form

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from .views import views
+from .auth import auth
 
 app = Flask(__name__)
 
+
 def initapp():
     app.register_blueprint(views, url_prefix="/")
-    
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
