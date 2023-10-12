f32eebc310b9b3f7167f833c05fa76d1d3ea4f05
Victor Chiaka <chiakavictor205@gmail.com>
2023-10-12 15:22:23 +0100

    created db and db connection, and secret key

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,29 @@
 from flask import Flask
 from .views import views
 from .auth import auth
+import random, string, os
+from dotenv import load_dotenv
+from .database import establish_sql_connection
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+establish_sql_connection()
 
 app = Flask(__name__)
 
 
+def generate_secret(length: int):
+    return "".join(random.choice(string.ascii_letters) for i in range(length))
+
+
 def initapp():
+    if not SECRET_KEY:
+        app.config["SECRET_KEY"] = generate_secret(random.choice(25, 45))
+    else:
+        app.config["SECRET_KEY"] = SECRET_KEY
+
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
---
+++ backend/database.py
@@ -0,0 +1,33 @@
+import os, uuid, psycopg2
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+
+db_connection = psycopg2.connect(
+    database=DB_NAME, host=DB_HOST, port=DB_PORT, user=DB_USER, password=DB_PASSWORD
+)
+
+
+def establish_sql_connection():
+    cursor = db_connection.cursor()
+
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS users (
+        id varchar(255) PRIMARY KEY,
+        username varchar(255),
+        email varchar(255),
+        password_hash varchar(455)
+    );
+    """
+    )
+
+    db_connection.commit()
+    return db_connection
