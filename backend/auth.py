cc658c85b6dd9e4c6045053c7194c1ee36c7cf70
Victor Chiaka <chiakavictor205@gmail.com>
2023-10-12 01:06:07 +0100

    formatted auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -2,9 +2,11 @@ from flask import Blueprint, request, render_template
 
 auth = Blueprint("auth", __name__)
 
+
 @auth.route("/signup", methods=["POST"])
 def signin():
     pass
+
 
 @auth.route("/login", methods=["POST"])
 def login():
