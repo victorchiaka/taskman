caa0f04f2136de45b9ac3c01fd2f52d1a79e4b45
victorchiaka <chiakavictor205@gmail.com>
2024-03-13 19:31:24 +0100

    ft: implemented the chartjs doughnut chat ui

---
+++ src/components/Statistics/DoughnutChart.jsx
@@ -0,0 +1,31 @@
+import { Doughnut } from "react-chartjs-2";
+import PropTypes from "prop-types";
+
+const DoughnutChart = ({ completedTasks, uncompletedTasks }) => {
+  const completedCount = completedTasks.length;
+  const uncompletedCount = uncompletedTasks.length;
+
+  const data = {
+    labels: ["Completed Tasks", "Uncompleted Tasks"],
+    datasets: [
+      {
+        data: [completedCount, uncompletedCount],
+        backgroundColor: ["#36A2EB", "#b91d47"],
+        hoverBackgroundColor: ["#36A2EB", "#b91d47"],
+      },
+    ],
+  };
+
+  return (
+    <div className="chart-container">
+      <Doughnut data={data} />
+    </div>
+  );
+};
+
+DoughnutChart.propTypes = {
+  completedTasks: PropTypes.array,
+  uncompletedTasks: PropTypes.array,
+};
+
+export default DoughnutChart;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import Chart from "chart.js/auto";
+import jsonData from "./data.json";
+import DoughnutChart from "./DoughnutChart";
+
+const StatisticsDropDown = () => {
+  return (
+    <select className="select-dropdown">
+      <option value="" hidden disabled selected>
+        {" "}
+        -- Select a Collection --
+      </option>
+      <option value="goals">Goals</option>
+      <option value="software-engineers">Software engineers</option>
+      <option value="school-related-goals">School related goals</option>
+      <option value="self-improvement">Self improvement</option>
+    </select>
+  );
+};
 
 const Statistics = () => {
+  const doughnutData = {
+    completedTasks: jsonData.completed_tasks,
+    uncompletedTasks: jsonData.uncompleted_tasks,
+  };
+
   return (
-    <div>Statistics</div>
-  )
-}
+    <>
+      <div className="instance-action">
+        <div>
+          View your tasks statistics: &nbsp;
+          <StatisticsDropDown />
+        </div>
+      </div>
+      <DoughnutChart {...doughnutData} />
+    </>
+  );
+};
 
 export default Statistics;
